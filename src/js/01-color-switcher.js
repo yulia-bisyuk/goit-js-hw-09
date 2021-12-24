fb4bdad6d83d76d2a45d7ac1f699fd8e29b26e75
yulia-bisyuk <yulia.bisyuk@gmail.com>
2021-12-25 01:02:53 +0200

    Task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+const refs = {
+    startBtn: document.querySelector('[data-start]'),
+    stopBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+let intervalId;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function changeBodyColor() {
+    
+    const currentColor = getRandomHexColor();
+    refs.body.style.backgroundColor = currentColor;
+}
+
+function onStartBtnClick() {
+
+    intervalId = setInterval(changeBodyColor, 1000);
+    refs.startBtn.setAttribute('disabled', true);
+    refs.stopBtn.removeAttribute('disabled');
+    
+}
+
+function onStopBtnClick() {
+
+    clearInterval(intervalId);
+    refs.stopBtn.setAttribute('disabled', true);
+    refs.startBtn.removeAttribute('disabled');
+    
+}
